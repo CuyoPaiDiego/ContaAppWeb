7b72e39bcf7df1cad62b70d1d6270ea285874a00
Pad28 <mipape2281003@gmail.com>
2024-06-28 00:16:52 -0600

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,16 @@
-import { useState } from 'react'
+import { Route, Routes } from "react-router-dom";
+import { RegisterStudentScreen, SendEmailScreen } from "./pages"
+import { Header } from "./components";
 
-import logo from './logos/logo-removebg-preview.png';
-
-import './themes/stylesGlobal.css'
-
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", }} >
-
-
-      <div className='header'>
-        <div className='contenedorLogoHeaderIzq'>
-          <img src={logo} className="logo" alt="Vite logo" />
-        </div>
-        <div className='contenedorTituloHeader'>
-          <h1 className='titulo'>Ingresa tus datos</h1>
-        </div>
-        <div className='contenedorLogoHeaderDer'>
-          {/* <img src={logo} className="logo" alt="Vite logo" /> */}
-        </div>
-      </div>
-
-      <div className='body'>
-        
-        <div className="form-container">
-          <div className="form-item">
-            <label htmlFor="nombre">Nombre</label>
-            <input type="text" id="nombre" placeholder="Ingrese su nombre" />
-          </div>
-          <div className="form-item">
-            <label htmlFor="apellidos">Apellidos</label>
-            <input type="text" id="apellidos" placeholder="Ingrese sus apellidos" />
-          </div>
-          <div className="form-item">
-            <label htmlFor="grupo">Grupo</label>
-            <input type="text" id="grupo" placeholder="Ingrese su grupo" />
-          </div>
-          <div className="form-item">
-            <label htmlFor="contrasena">Contraseña</label>
-            <input type="password" id="contrasena" placeholder="Ingrese su contraseña" />
-          </div>
-          <div className="form-item">
-            <label htmlFor="confirmar-contrasena">Confirmar Contraseña</label>
-            <input type="password" id="confirmar-contrasena" placeholder="Confirme su contraseña" />
-          </div>
-        </div>
-        
-      </div>
-      <div className="submit-button">
-        <button className='boton' type="button">Registrarme</button>
-      </div>
-
+    <div>
+      <Routes>
+        <Route path="/send-email" element={<SendEmailScreen />} />
+        <Route path="/register/:token" element={<RegisterStudentScreen />} />
+        <Route path="*" element={<h1>404 | Not found <div style={{ height: '28rem' }} /> </h1>} />
+      </Routes>
     </div>
   )
 }
-
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import Registrarse from '../src/screens/Registrarse.tsx'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom';
+import { App } from './App.tsx'
+
+import "./styles/global/normalize.css";
+import "./styles/global/index.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Registrarse />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
